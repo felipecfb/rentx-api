6cbdb7218c0cee5ef6dac08e5e913f7ec3fd4f85
Felipe Bastos <felipe.bastos.dev@gmail.com>
2023-04-03 11:26:37 -0300

    ✨ feat: adding cors

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -3,6 +3,7 @@ import 'dotenv/config';
 import 'express-async-errors';
 import '@shared/container';
 
+import cors from 'cors';
 import express, { NextFunction, Request, Response } from 'express';
 import swaggerUI from 'swagger-ui-express';
 
@@ -20,6 +21,8 @@ app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerFile));
 
 app.use('/avatar', express.static(`${upload.tmpFolder}/avatar`));
 app.use('/cars', express.static(`${upload.tmpFolder}/cars`));
+
+app.use(cors());
 
 app.use(router);
 
