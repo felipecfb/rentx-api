f8e134d9ade2923c3ac9aa0885650ffec6b22896
Felipe Bastos <felipe.bastos.dev@gmail.com>
2023-04-03 09:24:23 -0300

    ♻️ refactor: change the file for database connection

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,13 +1,7 @@
-import { AppDataSource } from '../typeorm/data-source';
+import { createConnection } from '../typeorm/data-source';
 import { app } from './app';
 
-AppDataSource.initialize()
-  .then(() => {
-    console.log('Data Source has been initialized!');
-  })
-  .catch(err => {
-    console.error('Error during Data Source initialization', err);
-  });
+createConnection();
 
 app.listen(3333, () => {
   console.log('Server is running on http://localhost:3333');
--- src/shared/infra/typeorm/data-source.ts
+++ src/shared/infra/typeorm/data-source.ts
@@ -1,27 +1,62 @@
+/* eslint-disable import/no-extraneous-dependencies */
 import 'dotenv/config';
 
 import 'reflect-metadata';
 import { DataSource, DataSourceOptions } from 'typeorm';
 import { SeederOptions } from 'typeorm-extension';
 
-const database =
-  process.env.NODE_ENV === 'test'
-    ? 'db_rentx_test'
-    : process.env.TYPEORM_DATABASE;
+function getAccessKeys(EnvVarMode: string) {
+  switch (EnvVarMode) {
+    case 'development':
+      return {
+        type: 'postgres',
+        host: process.env.TYPEORM_DEV_HOST,
+        port: Number(process.env.TYPEORM_DEV_PORT),
+        username: process.env.TYPEORM_DEV_USERNAME,
+        password: process.env.TYPEORM_DEV_PASSWORD,
+        database: process.env.TYPEORM_DEV_DATABASE,
+        entities: [`${process.env.TYPEORM_DEV_ENTITIES_PATH}`],
+        migrations: [`${process.env.TYPEORM_DEV_MIGRATIONS_PATH}`],
+        seeds: [`${process.env.TYPEORM_DEV_SEEDS_PATH}`],
+        synchronize: true,
+      };
+    case 'production':
+      return {
+        type: 'postgres',
+        host: process.env.TYPEORM_PROD_HOST,
+        port: Number(process.env.TYPEORM_PROD_PORT),
+        username: process.env.TYPEORM_PROD_USERNAME,
+        password: process.env.TYPEORM_PROD_PASSWORD,
+        database: process.env.TYPEORM_PROD_DATABASE,
+        entities: [`${process.env.TYPEORM_PROD_ENTITIES_PATH}`],
+        migrations: [`${process.env.TYPEORM_PROD_MIGRATIONS_PATH}`],
+        seeds: [`${process.env.TYPEORM_PROD_SEEDS_PATH}`],
+        synchronize: true,
+      };
+    default:
+      return null;
+  }
+}
+
+const setVariables = getAccessKeys(process.env.NODE_ENV);
 
 const options: DataSourceOptions & SeederOptions = {
   type: 'postgres',
-  host: process.env.TYPEORM_HOST,
-  port: Number(process.env.TYPEORM_PORT),
-  username: process.env.TYPEORM_USERNAME,
-  password: process.env.TYPEORM_PASSWORD,
-  database: database,
-  entities: ['./src/modules/**/entities/*.{ts,js}'],
-  migrations: ['./src/shared/**/migrations/*.{ts,js}'],
-  seeds: ['./src/shared/infra/typeorm/seeds/UserAdminSeeder.ts'],
+  host: setVariables.host,
+  port: Number(setVariables.port),
+  username: setVariables.username,
+  password: setVariables.password,
+  database: setVariables.database,
+  entities: setVariables.entities,
+  migrations: setVariables.migrations,
+  seeds: setVariables.seeds,
   synchronize: true,
 };
 
 const AppDataSource = new DataSource(options);
+
+export function createConnection(): Promise<DataSource> {
+  return AppDataSource.initialize();
+}
 
 export { AppDataSource };
